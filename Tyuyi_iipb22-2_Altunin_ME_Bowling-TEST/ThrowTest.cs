7a6b12509d8989f1e0c790d23d0f7a98102ade7c
Deboolf <81181986+Deboolf@users.noreply.github.com>
2023-10-11 23:18:45 +0500

    add tests

---
+++ Tyuyi_iipb22-2_Altunin_ME_Bowling-TEST/ThrowTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuyi_iipb22_2_Altunin_ME_Bowling;
+using System;
+
+namespace Tyuyi_iipb22_2_Altunin_ME_Bowling_TESTs
+{
+    [TestClass]
+    public class TrowTest
+    {
+        [TestMethod]
+        public void Throw_t()
+        {
+
+            Throw attempt = new Throw(5);
+            Assert.IsNotNull(attempt);
+            int score = attempt.Score;
+            Assert.AreEqual(5, score);
+
+        }
+    }
+}
+
