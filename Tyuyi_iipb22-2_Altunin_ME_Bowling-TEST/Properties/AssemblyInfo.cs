b48fe8dcc534c3fd13487b20e103c3f45d539f03
Deboolf <81181986+Deboolf@users.noreply.github.com>
2023-10-11 23:09:10 +0500

    second commit

---
+++ Tyuyi_iipb22-2_Altunin_ME_Bowling-TEST/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuyi_iipb22-2_Altunin_ME_Bowling-TEST")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuyi_iipb22-2_Altunin_ME_Bowling-TEST")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ea8dcc20-6d3f-4aec-81b8-70215f849a1a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuyi_iipb22-2_Altunin_ME_Bowling-TEST/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tyuyi_iipb22_2_Altunin_ME_Bowling_TEST
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
